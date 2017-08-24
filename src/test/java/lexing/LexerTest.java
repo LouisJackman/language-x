63e7753bbf09c1136c8fc663198f2fcb67b38855
Louis Jackman <LouisJackman@users.noreply.github.com>
2017-08-24 22:00:37 +0100

    Revert the interpolated string change; it's too verbose and clumsy to write

--- src/test/java/lexing/LexerTest.java
+++ src/test/java/lexing/LexerTest.java
@@ -64,7 +64,8 @@ public class LexerTest extends TestCase {
 
     public void testInterpolatedStrings() {
         // TODO: test actual interpolation once the parser is complete.
-        Lexer lexer = createLexer("   $\"123\"   $\"abc\"");
+
+        Lexer lexer = createLexer("   `123`   `abc`");
         assertEquals("123", lexer.next().getValue().get());
         assertEquals("abc", lexer.next().getValue().get());
     }
