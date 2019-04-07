8f978c770baf5949b2967f54a8b2805056e4523d
LouisJackman <LouisJackman@users.noreply.github.com>
2019-04-07 11:07:42 +0100

    Try CircleCI for consistency with other projects; it should fail due to lack of codecov token

---
+++ scripts/codecov.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+report=$(find target/debug -maxdepth 1 -name 'sylan-*' -a ! -name '*.d')
+
+for file in $report
+do
+    mkdir -p "target/cov/$(basename $file)"
+    kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"
+done
+
+wget -O - -q "https://codecov.io/bash" >.codecov
+chmod +x .codecov
+./.codecov -t "$CODECOV_TOKEN"
+
